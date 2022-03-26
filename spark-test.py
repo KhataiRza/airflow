acd83567d14eebac032140ab59087212a820850f
KhataiRza <82077172+KhataiRza@users.noreply.github.com>
2022-03-27 00:44:44 +0400

    Update spark-test.py

--- spark-test.py
+++ spark-test.py
@@ -1,4 +1,4 @@
-#test1
+
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
@@ -51,4 +51,3 @@ spark_job = SparkSubmitOperator(
 end = DummyOperator(task_id="end", dag=dag)
 
 start >> spark_job >> end
-#test1
